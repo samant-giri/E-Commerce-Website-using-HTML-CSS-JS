eedb66e9e1791f594235ecaca029452a54532f8b
Samant Giri <mahakalsamant001@gmail.com>
2024-01-04 00:02:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// Loops in JavaScript
+
+
+let animal = {
+    name1: "dog",
+    name2: 34,
+    name3: "elephant",
+    name4 : "ant"
+}
+
+console.log(typeof(animal.name2));
+
+// console.log(animal.name1)
+
+// for (let i in animal) {
+//     console.log(i, animal[i]);
+// }
+
+// let arr = ["samant", "ram bahadur", "sachin", "rishabh pal", "rishabh"]
+// for (let j in arr) {
+//     console.log(arr[j])
+// }
+// for (let j of arr) {
+//     console.log(j)
+// }
+
+// let x = "samantgiri"
+// for (i of x) {
+//     console.log(i)
+// }
+
+
+// Functions in javascript
+
+
+function sum(x, y) {
+    console.log(x+y)
+}
+
+sum(4, 5)
+
+console.log("samant")
